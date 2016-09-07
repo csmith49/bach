bf7e9b60b8bbceed738c47ac62398772bd398f36
Aws Albarghouthi <albargah@gmail.com>
2016-09-07 07:23:36 -0500

    example where inputs/outputs returns empty list

--- eval.ml
+++ eval.ml
@@ -1,4 +1,5 @@
 open Core
+open Printf
 
 let noisy = ref false
 
@@ -11,7 +12,15 @@ let anon_fun s = raise (Arg.Bad (s ^ " is not recognized"))
 
 let _ =
     let t = Sys.time() in
+
     Arg.parse (Arg.align spec_list) anon_fun usage_msg;
+
+    let s = Symbol ("R", ["int";"int"]) in
+    let r = Relation ("R", ["X,Y"]) in
+    let c = Cube [r] in
+    (* XXX: bug *)
+    printf "# of Inputs: %d\n" (List.length (inputs c));
+    print_endline ("This is a symbol: " ^ (string_of_symbol s));
 
     if !noisy then begin
         print_endline "\n==> STATS ==>";
