bcadf858690c0ad9a3f986c85373df1a4e5ce9e0
Calvin Smith <calvin@wifi-82.cs.wisc.edu>
2016-10-03 15:33:56 -0500

    we got a loop now

--- eval.ml
+++ eval.ml
@@ -1,5 +1,6 @@
 open Core
 open Checker
+open Search
 open Printf
 
 let noisy = ref false
@@ -12,21 +13,36 @@ let usage_msg = "todo"
 let anon_fun s = raise (Arg.Bad (s ^ " is not recognized"))
 
 let _ =
+    let f = ref (Search.start Partition.empty) in
     let t = Sys.time() in
+    let wrapped_check l r = check !global_config.signature l r true in
+    let seen = ref [] in
+    let to_cube p = List.map snd (RelMap.bindings p) in
 
     Arg.parse (Arg.align spec_list) anon_fun usage_msg;
 
-    let s = Symbol ("R", ["int";"int"]) in
-    let r = Relation ("R", ["X";"Y"]) in
-    let c = Cube [r] in
+    for i = 0 to 100 do
+        (* get the newest element and update the frontier *)
+        let e, fp = Search.next !f in
+        f := fp;
+        (* let's see what we're working with *)
+        print_endline (string_of_cube (Partition.flatten e));
+        (* for every already-seen cube, we'll make an equation and check *)
+        List.iter (fun c -> begin
+            wrapped_check (to_cube c) (to_cube e); ()
+        end) !seen;
+        (* and then update our list of seen cubes *)
+        seen := e :: !seen;
+    done;
 
-    (* XXX: bug *)
-    printf "# of Inputs: %d\n" (List.length (inputs c));
-    print_endline ("This is a symbol: " ^ (string_of_symbol s));
-
-    let m = check [s] [c;c] [c] true in
-    
+    (* now we wrap up by printing stats that we care about *)
     if !noisy then begin
         print_endline "\n==> STATS ==>";
         Printf.printf "time %fs\n" (Sys.time() -. t);
     end;
+
+    for i = 0 to 10 do
+        let e, fp = Search.next !f in
+        f := fp;
+        print_endline (string_of_cube (Partition.flatten e));
+    done;
