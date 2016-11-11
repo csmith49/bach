4d264977342ea839bdc58d0fde22fb2d5debe0f8
Calvin Smith <calvin@wifi-605.cs.wisc.edu>
2016-11-11 12:27:44 -0600

    lots of print statements

--- src/preds.ml
+++ src/preds.ml
@@ -1,5 +1,7 @@
 open Abduction
 
-let _ = register_predicate "eq0" ["fp17"] (fun xs -> (List.hd xs) = "0");
+let _ = register_predicate "eq0" ["fp17"] (fun xs -> (List.hd xs) = "0")
+let _ = register_predicate "true" ["bool"] (fun xs -> (List.hd xs) = "True")
+let _ = register_predicate "false" ["bool"] (fun xs -> (List.hd xs) = "False")
 
-print_endline "Predicates registered.";
+let _ = print_endline "Predicates registered."
