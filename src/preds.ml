8d1011f9784b22b6910bc6e0f7a3f70e1fa1c844
Aws Albarghouthi <albargah@gmail.com>
2016-11-12 17:17:05 -0600

    preds

--- src/preds.ml
+++ src/preds.ml
@@ -3,7 +3,13 @@ open Abduction
 let _ = register_predicate "eq0" ["fp17"] (fun xs -> (List.hd xs) = "0")
 let _ = register_predicate "true" ["bool"] (fun xs -> (List.hd xs) = "True")
 let _ = register_predicate "false" ["bool"] (fun xs -> (List.hd xs) = "False")
-let _ = register_predicate "error" ["tensor"] (fun xs -> (List.hd xs) = "Error")
+let _ = register_predicate "err-tensor" ["tensor"] (fun xs -> (List.hd xs) = "Error")
+let _ = register_predicate "identity" ["tensor"] (fun xs -> (List.hd xs) = "(1, 0, 0, 1)")
+let _ = register_predicate "gte-1" ["int"] (fun xs -> (int_of_string (List.hd xs)) >= -1)
+let _ = register_predicate "gt" ["int";"int"] (fun xs -> (int_of_string (List.nth xs 0)) > (int_of_string (List.nth xs 1)) )
+let _ = register_predicate "lte1" ["int"] (fun xs -> (int_of_string (List.hd xs)) <= 1)
+let _ = register_predicate "invr" ["rad";"rad"] (fun xs -> (int_of_string (List.nth xs 0)) = -1*(int_of_string (List.nth xs 1)))
+let _ = register_predicate "invi" ["int";"int"] (fun xs -> (int_of_string (List.nth xs 0)) = -1*(int_of_string (List.nth xs 1)))
 let _ = register_predicate "m1" ["rad"; "rad"] (
    
     fun xs ->  
