4c0c6f21e822bbdc3bc2f6bb3f3852ce45cd0c18
Aws Albarghouthi <albargah@gmail.com>
2016-11-13 18:10:58 -0600

    more preds

--- src/preds.ml
+++ src/preds.ml
@@ -5,18 +5,34 @@ let _ = register_predicate "true" ["bool"] (fun xs -> (List.hd xs) = "True")
 let _ = register_predicate "false" ["bool"] (fun xs -> (List.hd xs) = "False")
 let _ = register_predicate "err-tensor" ["tensor"] (fun xs -> (List.hd xs) = "Error")
 let _ = register_predicate "identity" ["tensor"] (fun xs -> (List.hd xs) = "(1, 0, 0, 1)")
-let _ = register_predicate "gte-1" ["int"] (fun xs -> (int_of_string (List.hd xs)) >= -1)
+(*let _ = register_predicate "gte" ["int";"int"] (fun xs -> (int_of_string (List.nth xs 0)) >= (int_of_string (List.nth xs 1)) )
+*)
+(*let _ = register_predicate "gte-1" ["int"] (fun xs -> (int_of_string (List.hd xs)) >= -1)
 let _ = register_predicate "gt" ["int";"int"] (fun xs -> (int_of_string (List.nth xs 0)) > (int_of_string (List.nth xs 1)) )
+
 let _ = register_predicate "lte1" ["int"] (fun xs -> (int_of_string (List.hd xs)) <= 1)
+
 let _ = register_predicate "invr" ["rad";"rad"] (fun xs -> (int_of_string (List.nth xs 0)) = -1*(int_of_string (List.nth xs 1)))
 let _ = register_predicate "invi" ["int";"int"] (fun xs -> (int_of_string (List.nth xs 0)) = -1*(int_of_string (List.nth xs 1)))
-let _ = register_predicate "m1" ["rad"; "rad"] (
-
+*)
+let _ = register_predicate "empty_dict" ["dict"] (fun xs -> (List.nth xs 0) = "{}")
+let _ = register_predicate "empty_list" ["list"] (fun xs -> (List.nth xs 0) = "[]")
+let _ = register_predicate "twoPiApart" ["rad"; "rad"] (
     fun xs ->
     (*fun xs -> if (((List.nth xs 0) = "Error") ||  ((List.nth xs 1) = "Error")) then
         true
     else*)
         let x = (int_of_string (List.nth xs 0)) in
         let y = (int_of_string (List.nth xs 1)) in
-        (x - y) mod 2 = 0
+        (x - y) mod 4 = 0
+)
+
+let _ = register_predicate "halfPiApart" ["rad"; "rad"] (
+    fun xs ->
+    (*fun xs -> if (((List.nth xs 0) = "Error") ||  ((List.nth xs 1) = "Error")) then
+        true
+    else*)
+        let x = (int_of_string (List.nth xs 0)) in
+        let y = (int_of_string (List.nth xs 1)) in
+        (x + y) = 1
 )
